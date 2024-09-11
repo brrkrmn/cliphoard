3da709759a67e8254fa97a37312edd794a93b6d1
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-09-11 15:50:00 +0300

    set up tailwindcss

--- webpack.config.js
+++ webpack.config.js
@@ -28,8 +28,9 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
     ],
   },
+  watch: true,
 };
