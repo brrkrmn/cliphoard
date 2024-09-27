a29cdce400c2358047ead925974d4669ca4b0807
Berra Karaman <esma.berra.karaman@hotmail.com>
2024-09-27 17:49:01 +0300

    refactor header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,10 @@
-import Logo from "./components/Logo/Logo";
-
 const Header = () => {
   return (
     <div className="section justify-between z-50">
-      <Logo />
+      <div className="flex items-center justify-center gap-2" >
+        <img className="w-8 h-8" src="./icons/cliphoard.png" />
+        <p className="font-bold text-black">ClipHoard</p>
+      </div>
     </div>
   )
 }
